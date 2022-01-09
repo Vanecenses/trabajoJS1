c79c548af8185e41ac04383d601b533f5f7def56
vanesa vidal <vanesa.fatima.vidal22@gmail.com>
2022-01-09 13:23:27 -0300

    subir js

---
+++ index.js
@@ -0,0 +1,14 @@
+function calcularedad(nombre,edad){
+    var edad = document.getElementById("edad").value;
+    
+    if(edad>=18){
+        alert(" eres mayor de edad");
+
+    }else if(edad<=17){
+        alert("eres menor de edad");
+        
+    }else(edad>=60);{
+        alert("estas en la mayoria de edad");
+    }
+
+}
